499a3bc9d16e23ae93210777f1bf3aa71064ca0f
Felix Kaiser <mail@felixkaiser.com>
2014-01-29 18:34:20 +0100

    Initial version

---
+++ GoogleTagManager.i18n.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Internationalisation file for extension GoogleTagManager.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Felix Kaiser
+ */
+$messages['en'] = array(
+	'googletagmanager-desc' => 'Inserts a Google Tag Manager container into MediaWiki pages for tracking',
+);
+
+/** Message documentation (Message documentation)
+ * @author Felix Kaiser
+ */
+$messages['qqq'] = array(
+	'googletagmanager-desc' => 'The name of the extension\'s entry in Special:SpecialPages',
+);
